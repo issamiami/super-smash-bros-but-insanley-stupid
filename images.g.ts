d0caf4df84e9a3ab765416955e3962da0c49481e
OLegonD <75202965+OLegonD@users.noreply.github.com>
2022-11-09 17:20:33 +0100

    Animations

---
+++ images.g.ts
@@ -0,0 +1,41 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+....................
+....................
+..f....fffffff......
+.f3f..f3333333f.....
+.f3f.f33333333f.....
+.f3ff3333333333f....
+.f3f333333333333f...
+.f3333f133331f333f..
+.f333333333333333f..
+..f33fff333ff3333ff.
+..f33f22fff22f3333f.
+..f333f22222f33333f.
+..f3333f111f333333f.
+..f33333fff3333333f.
+...f3333333333fff3f.
+....f332222233f.f3f.
+.....ff2fff2ff..f3f.
+.......2...2.....f..
+.....222...222......
+.....111...111......
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
